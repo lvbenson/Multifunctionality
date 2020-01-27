4cce0fc35d7cf485a787f9a34d5c8529cf511e4f
Lauren Benson <lvbenson@iu.edu>
2020-01-27 17:52:45 -0500

    impl. mountain car (continuous) control task

---
+++ mountaincar_cont.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 27 17:26:23 2020
+
+@author: Lauren Benson
+
+"""
+
+import math
+import numpy as np
+import ctrnn
+
+
+#nervous system
+
+
+
+#constants
+
+
+
+#Neuron Roles
+
+
+
+class MountainCarAgent:
+    
+    def __init__()
+
