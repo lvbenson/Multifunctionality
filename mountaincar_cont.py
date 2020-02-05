7e4619de6da5d556353d2e1fbfcdeeea3e25a17f
Lauren Benson <lvbenson@iu.edu>
2020-02-05 17:43:33 -0500

    update step function

--- mountaincar_cont.py
+++ mountaincar_cont.py
@@ -11,19 +11,37 @@ import numpy as np
 import ctrnn
 
 
-#nervous system
-
-
-
-#constants
-
-
-
-#Neuron Roles
-
-
-
 class MountainCarAgent:
-    
-    def __init__()
+    def __init__(self, goal_velocity = 0):
+        self.min_action = -1.0
+        self.max_action = 1.0
+        self.min_position = -1.2
+        self.max_position = 0.6
+        self.max_speed = 0.07
+        self.goal_position = 0.45 # was 0.5 in gym, 0.45 in Arnaud de Broissia's version
+        self.goal_velocity = goal_velocity
+        self.power = 0.0015
+
+        self.low_state = np.array([self.min_position, -self.max_speed])
+        self.high_state = np.array([self.max_position, self.max_speed])
+        
+        
+    def step(self,stepsize,action):
+        position = self.state[0]
+        velocity = self.state[1]
+        force = min(max(action[0], -1.0), 1.0)
+        velocity += force*self.power -0.0025 * math.cos(3*position)
+        if (velocity > self.max_speed): velocity = self.max_speed
+        if (velocity < -self.max_speed): velocity = -self.max_speed
+        position += velocity
+        if (position > self.max_position): position = self.max_position
+        if (position < self.min_position): position = self.min_position
+        if (position==self.min_position and velocity<0): velocity = 0
+        done = bool(position >= self.goal_position and velocity >= self.goal_velocity)
+        if not done:
+            return 100.0 * stepsize 
+        else:
+            return 0.0
+
+
 
