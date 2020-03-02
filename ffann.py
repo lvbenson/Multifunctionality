171cdef27eaf1f4a0e098319dcaef561beb14358
Lauren Benson <lvbenson@iu.edu>
2020-03-02 10:02:56 -0500

    intolesion

--- ffann.py
+++ ffann.py
@@ -1,5 +1,4 @@
 import numpy as np
-import viz_infolesion
 
 # According to conversations with Abe,
 # the optimal behavior should be around
@@ -8,33 +7,16 @@ import viz_infolesion
 # the normalized optimal cost should be 0.65.
 # I'm getting closer to 0.94 (188).
 
-
-#Lauren Benson 
-#Goal: Comment, figure out script, eventually evolve ffnn to solve all tasks 
-
-
 def sigmoid(x):
-    return 1/(1 + np.exp(-x)) #sigmoid required for activation function 
-
-
+    return 1/(1 + np.exp(-x))
 
 def relu(x):
     return x * (x > 0)
 
 class ANN:
 
-#h = output
-#i = input 
-#b = bias
-#n = neuron
-#w = weight     
-#WHAT IS U??? 
-#Weight(layer number) 
-    
-    
     def __init__(self, NIU, NH1U, NH2U, NOU):
-        #neuron input
-        self.nI = NIU 
+        self.nI = NIU #input
         self.nH1 = NH1U
         self.nH2 = NH2U
         self.nO = NOU
@@ -49,122 +31,69 @@ class ANN:
         self.OutputActivation = np.zeros(NOU)
         self.Input = np.zeros(NIU)
 
-    def setParametersSTD(self, genotype, std):
-        k = 0
-        for i in range(self.nI):
-            for j in range(self.nH1):
-                self.wIH1[i][j] = genotype[k]*(std/(self.nI*self.nH1))
-                k += 1
-        for i in range(self.nH1):
-            for j in range(self.nH2):
-                self.wH1H2[i][j] = genotype[k]*(std/(self.nH1*self.nH2))
-                k += 1
-        for i in range(self.nH2):
-            for j in range(self.nO):
-                self.wH2O[i][j] = genotype[k]*(std/(self.nH2*self.nO))
-                k += 1
-        for i in range(self.nH1):
-            self.bH1[i] = genotype[k]*(std/self.nH1)
-            k += 1
-        for i in range(self.nH2):
-            self.bH2[i] = genotype[k]*(std/self.nH2)
-            k += 1
-        for i in range(self.nO):
-            self.bO[i] = genotype[k]*(std/self.nO)
-            k += 1
-
-    def setParameters(self, genotype, WeightRange, BiasRange):
-        k = 0
-        for i in range(self.nI):
-            for j in range(self.nH1):
-                self.wIH1[i][j] = genotype[k]*WeightRange
-                k += 1
-        for i in range(self.nH1):
-            for j in range(self.nH2):
-                self.wH1H2[i][j] = genotype[k]*WeightRange
-                k += 1
-        for i in range(self.nH2):
-            for j in range(self.nO):
-                self.wH2O[i][j] = genotype[k]*WeightRange
-                k += 1
-        for i in range(self.nH1):
-            self.bH1[i] = genotype[k]*BiasRange
-            k += 1
-        for i in range(self.nH2):
-            self.bH2[i] = genotype[k]*BiasRange
-            k += 1
-        for i in range(self.nO):
-            self.bO[i] = genotype[k]*BiasRange
-            k += 1
-
 # =============================================================================
-#     def ablate(self, neuron): # Set outgoing connections to 0
-#         if (neuron<self.nI):
-#             i = neuron
+#     def setParametersSTD(self, genotype, std):
+#         k = 0
+#         for i in range(self.nI):
 #             for j in range(self.nH1):
-#                 self.wIH1[i][j] = 0.0
-#         if (neuron >= self.nI and neuron < self.nI+self.nH1):
-#             i = neuron-self.nI
+#                 self.wIH1[i][j] = genotype[k]*(std/(self.nI*self.nH1))
+#                 k += 1
+#         for i in range(self.nH1):
 #             for j in range(self.nH2):
-#                 self.wH1H2[i][j] = 0.0
-#         if (neuron >= self.nI+self.nH1 and neuron < self.nI+self.nH1+self.nH2):
-#             i = neuron-(self.nI+self.nH1)
+#                 self.wH1H2[i][j] = genotype[k]*(std/(self.nH1*self.nH2))
+#                 k += 1
+#         for i in range(self.nH2):
 #             for j in range(self.nO):
-#                 self.wH2O[i][j] = 0.0
-#     
-# =============================================================================
-    
-    def ablate_infolesion_ip(self, neuron):
-        ip_lesion_1 = np.load('ip_hidden_1.npy')
-        ip_lesion_2 = np.load('ip_hidden_2.npy')
+#                 self.wH2O[i][j] = genotype[k]*(std/(self.nH2*self.nO))
+#                 k += 1
+#         for i in range(self.nH1):
+#             self.bH1[i] = genotype[k]*(std/self.nH1)
+#             k += 1
+#         for i in range(self.nH2):
+#             self.bH2[i] = genotype[k]*(std/self.nH2)
+#             k += 1
+#         for i in range(self.nO):
+#             self.bO[i] = genotype[k]*(std/self.nO)
+#             k += 1
+# 
+    def setParameters(self, genotype, WeightRange, BiasRange):
+         k = 0
+         for i in range(self.nI):
+             for j in range(self.nH1):
+                 self.wIH1[i][j] = genotype[k]*WeightRange
+                 k += 1
+         for i in range(self.nH1):
+             for j in range(self.nH2):
+                 self.wH1H2[i][j] = genotype[k]*WeightRange
+                 k += 1
+         for i in range(self.nH2):
+             for j in range(self.nO):
+                 self.wH2O[i][j] = genotype[k]*WeightRange
+                 k += 1
+         for i in range(self.nH1):
+             self.bH1[i] = genotype[k]*BiasRange
+             k += 1
+         for i in range(self.nH2):
+             self.bH2[i] = genotype[k]*BiasRange
+             k += 1
+         for i in range(self.nO):
+             self.bO[i] = genotype[k]*BiasRange
+             k += 1
+ 
+    def ablate(self, neuron): # Set outgoing connections to 0
         if (neuron<self.nI):
             i = neuron
-            for j in range(self.nHI):
-                    self.wIH1[i][j] = ip_lesion_1[j]
+            for j in range(self.nH1):
+                self.wIH1[i][j] = 0.0
         if (neuron >= self.nI and neuron < self.nI+self.nH1):
             i = neuron-self.nI
             for j in range(self.nH2):
-                self.wH1H2[i][j] = ip_lesion_2[j]
+                self.wH1H2[i][j] = 0.0
         if (neuron >= self.nI+self.nH1 and neuron < self.nI+self.nH1+self.nH2):
             i = neuron-(self.nI+self.nH1)
             for j in range(self.nO):
                 self.wH2O[i][j] = 0.0
-        
-    def ablate_infolesion_cp(self, neuron):
-        cp_lesion_1 = np.load('cp_hidden_1.npy')
-        cp_lesion_2 = np.load('cp_hidden_2.npy')
-        if (neuron<self.nI):
-            i = neuron
-            for j in range(self.nHI):
-                    self.wIH1[i][j] = cp_lesion_1[j]
-        if (neuron >= self.nI and neuron < self.nI+self.nH1):
-            i = neuron-self.nI
-            for j in range(self.nH2):
-                self.wH1H2[i][j] = cp_lesion_2[j]
-        if (neuron >= self.nI+self.nH1 and neuron < self.nI+self.nH1+self.nH2):
-            i = neuron-(self.nI+self.nH1)
-            for j in range(self.nO):
-                self.wH2O[i][j] = 0.0
-        
-    def ablate_infolesion_lw(self, neuron):
-        lw_lesion_1 = np.load('lw_hidden_1.npy')
-        lw_lesion_2 = np.load('lw_hidden_2.npy')
-        if (neuron<self.nI):
-            i = neuron
-            for j in range(self.nHI):
-                    self.wIH1[i][j] = lw_lesion_1[j]
-        if (neuron >= self.nI and neuron < self.nI+self.nH1):
-            i = neuron-self.nI
-            for j in range(self.nH2):
-                self.wH1H2[i][j] = lw_lesion_2[j]
-        if (neuron >= self.nI+self.nH1 and neuron < self.nI+self.nH1+self.nH2):
-            i = neuron-(self.nI+self.nH1)
-            for j in range(self.nO):
-                self.wH2O[i][j] = 0.0
-        
-    
-    
-
+                
     def step(self,Input):
         self.Input = np.array(Input)
         self.Hidden1Activation = relu(np.dot(self.Input.T,self.wIH1)+self.bH1)
@@ -173,7 +102,10 @@ class ANN:
         return self.OutputActivation
 
     def output(self):
+        #print("output",self.OutputActivation)
+        #print("ffann output",self.OutputActivation*2 - 1)
         return self.OutputActivation*2 - 1
+    
 
     def states(self):
         return np.concatenate((self.Input,self.Hidden1Activation,self.Hidden2Activation,self.OutputActivation))
